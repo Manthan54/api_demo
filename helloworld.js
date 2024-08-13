b05cf3a73cb043fe6fb22044c6df4335572c0d0d
Manthan Chandravijay Jagtap <133759783+Manthan54@users.noreply.github.com>
2024-08-13 21:13:51 +0530

    Delete helloworld.js

--- helloworld.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context){
-    const {request} = context;
-    const {method} = request;
-    const data=null;
-    if (method === "POST") {
-        data = await request.text();
-        return new Response(`Hello ${data}`);
-    }
-}
