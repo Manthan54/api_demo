340f019150af41050e086b81eb0d087f6c606ac9
Manthan Chandravijay Jagtap <133759783+Manthan54@users.noreply.github.com>
2024-08-13 21:40:39 +0530

    Add files via upload

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,7 +1,7 @@
 export async function onRequest(context){
     const {request} = context;
     const {method} = request;
-    const data=null;
+    // const data=null;
     if (method === "POST") {
         data = await request.text();
         return new Response(`Hello ${data}`);
